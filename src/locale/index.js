d6947f538d42fb1110558a384d26201760190ebc
xinlaiW*8 <xinlai3698@163.com>
2019-11-15 15:46:35 +0800

    去掉注释

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,14 +1,6 @@
-/*
- * @Author: comer
- * @Date: 2019-11-15 14:32:51
- * @LastEditors: comer
- * @LastEditTime: 2019-11-15 14:35:31
- * @Email: xinlai3698@163.com
- * @Desc: 
- */
-import zhCN from './lang/zh-CN'
+import zhCN from './lang/zh-CN' 
 
 
-export default { 
+export default {   
   'zh-CN': zhCN
-}
+}  
